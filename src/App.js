104894f96e7659dcda81fc7b07b05843e8506de8
cyprste2717218 <smi505@york.ac.uk>
2023-03-30 00:45:44 +0100

    added social links in footer and changed design responsiveness

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import styles from './styles.css';
 import Rot13 from './caesarsCipherDecoder';
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLockOpen } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+
 
 export default class App extends Component {
 
@@ -21,22 +27,38 @@ export default class App extends Component {
   render() {
     return (
     
-      <div className="App" >
+      <div className="App appFont" >
         <div style={{minHeight: "80vh"}} >
           <Navbar bg="primary" variant="dark">
-            <Container>
-              <Navbar.Brand href="#home">Caesars Cipher Decoder App</Navbar.Brand>
+            <Container style={{color: "#fff"}}>
+              <Row>
+                
+                <Col xs={2} sm={1} md={1} lg={1} xl={2}>
+                  <br></br>
+                  <FontAwesomeIcon size='xl' icon={faLockOpen} />
+                </Col>
+                <Col xs={10} sm={11} md={11} lg={11} xl={2}>
+                  <br></br>
+                  <Navbar.Brand style={{fontSize: "1em"}} href="#home">Caesars Cipher Decoder App</Navbar.Brand>
+                </Col>
+             
+           
+              </Row>
+          
             </Container>
           </Navbar>
-    
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-3 col-xs-1 col-sm-1"></div>
-              <div class="col-lg-6 col-xs-10 col-sm-10">
-                <br></br>
+
+          
+          <Container>
+            <Row className="justify-content-center">
+              <Col xl={3} lg={3} md={1}  sm={2} xs={1}></Col> 
+              <Col xl={6}lg={6} md={10} sm={8} xs={9} >
+        
                 <br></br>
                 <br></br>
                 <p>The purpose of this app is to decode a Caesars Cipher ROT13 encoded string which you can supply as input below</p>
+                <hr></hr>
+                <br></br>
                 <br></br>
                 <InputGroup size="lg">
                   <Form.Label style={{width: "100%", textAlign: "left"}} htmlFor="caesar-cipher-encoded-string-input">Enter String Here:</Form.Label>
@@ -49,7 +71,7 @@ export default class App extends Component {
                 </InputGroup>
                 <br></br>
                 <br></br>
-                <br></br>
+          
                 <br></br>
                 <InputGroup size="lg">
                   <Form.Label style={{width: "100%", textAlign: "left"}} htmlFor="caesar-cipher-encoded-string-output">Output:</Form.Label>
@@ -61,20 +83,58 @@ export default class App extends Component {
                 <br></br>
                 <br></br>
                 
-              </div>
-              <div class="col-lg-3 col-xs-1 col-sm-1"></div>
+              </Col>
+              <Col xl={3} lg={3} md={1} sm={2} xs={1}></Col>
             
-            </div>
+            </Row>
             
-          </div>
+          </Container>
+        
         </div>
 
         <footer>
-          <div className="d-flex justify-content-space-between footer">
+          
+          <div style={{textAlign: "left"}} className="d-flex justify-content-space-between footer">
             <br></br>
             <br></br>
-            <p>Developed by <a className="footerTextLink" href="https://github.com/cyprste2717218">@cyprste2717218</a></p>
+            <Container>
+              <Row>
+               
+                <Col xs={7} sm={8} md={9} lg={9} xl={10}> 
+                  <p>Developed by <a className="footerTextLink" href="https://github.com/cyprste2717218">@cyprste2717218</a></p>
+                </Col>
+                <Col xs={5} sm={4} md={3} lg={3} xl={2}>
+                  <Container>
+                    <Row>
+                     
+                      <Col xs={6} sm={6} md={6} lg={6} xl={6}>
+                        <a  style={{color: "#fff"}} href="https://github.com/cyprste2717218">
+                          <FontAwesomeIcon className='icon' size='2xl'  icon={faGithub} />
+                        
+                        </a>
+                      </Col>
+                    
+                      <Col xs={6} sm={6} md={6} lg={6} xl={6}> 
+                        <a  style={{color: "#fff"}} href="https://www.linkedin.com/in/stephen-ingham-/">
+                            <FontAwesomeIcon className='icon' size='2xl'  icon={faLinkedin} />
+                           
+                        </a>
+                      </Col>
+                     
+
+                    </Row>
+
+                  </Container>
+
+                </Col>
+         
+
+
+              </Row>
+              
+            </Container>
           </div>
+      
         </footer>
    
       </div>

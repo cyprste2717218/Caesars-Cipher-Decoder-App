05bff1c7b5ff8f9c3c45feaaf6ea5499d34a35fb
cyprste2717218 <smi505@york.ac.uk>
2023-03-29 21:07:03 +0100

    improved design so provides a consistent design across viewports

--- src/App.js
+++ src/App.js
@@ -21,56 +21,62 @@ export default class App extends Component {
   render() {
     return (
     
-      <div className="App">
-        <Navbar bg="primary" variant="dark">
-          <Container>
-            <Navbar.Brand href="#home" style={{fontSize: "1.5em"}}>Caesars Cipher Decoder App</Navbar.Brand>
-          </Container>
-        </Navbar>
-  
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-3 col-xs-1 col-sm-1"></div>
-            <div class="col-lg-6 col-xs-10 col-sm-10">
-              <br></br>
-              <br></br>
-              <br></br>
-              <p style={{fontSize:"1.3em"}}>The purpose of this app is to decode a Caesars Cipher ROT13 encoded string which you can supply as input below</p>
-              <br></br>
-              <InputGroup size="lg">
-                <Form.Label style={{width: "100%", textAlign: "left", fontSize: "1em"}} htmlFor="caesar-cipher-encoded-string-input">Enter String Here:</Form.Label>
-                <Form.Control id="textInput"
-                  aria-label="Large"
-                  aria-describedby="inputGroup-sizing-sm"
-                  value={this.state.val}
-                  onChange={e => this.setState({ val: e.target.value })}
-                />
-              </InputGroup>
-              <br></br>
-              <br></br>
-              <br></br>
-              <br></br>
-              <InputGroup size="lg">
-                <Form.Label style={{width: "100%", textAlign: "left", fontSize: "1em"}} htmlFor="caesar-cipher-encoded-string-output">Output:</Form.Label>
-                <Rot13 str={this.state.val}></Rot13>
-              </InputGroup>
-              
-              <br></br>
-              <br></br>
-              <br></br>
-              <br></br>
-              
+      <div className="App" >
+        <div style={{minHeight: "80vh"}}>
+          <Navbar bg="primary" variant="dark">
+            <Container>
+              <Navbar.Brand href="#home" style={{fontSize: "1.5em"}}>Caesars Cipher Decoder App</Navbar.Brand>
+            </Container>
+          </Navbar>
+    
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-3 col-xs-1 col-sm-1"></div>
+              <div class="col-lg-6 col-xs-10 col-sm-10">
+                <br></br>
+                <br></br>
+                <br></br>
+                <p style={{fontSize:"1.3em"}}>The purpose of this app is to decode a Caesars Cipher ROT13 encoded string which you can supply as input below</p>
+                <br></br>
+                <InputGroup size="lg">
+                  <Form.Label style={{width: "100%", textAlign: "left", fontSize: "1em"}} htmlFor="caesar-cipher-encoded-string-input">Enter String Here:</Form.Label>
+                  <Form.Control id="textInput"
+                    aria-label="Large"
+                    aria-describedby="inputGroup-sizing-sm"
+                    value={this.state.val}
+                    onChange={e => this.setState({ val: e.target.value })}
+                  />
+                </InputGroup>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <InputGroup size="lg">
+                  <Form.Label style={{width: "100%", textAlign: "left", fontSize: "1em"}} htmlFor="caesar-cipher-encoded-string-output">Output:</Form.Label>
+                  <Rot13 str={this.state.val}></Rot13>
+                </InputGroup>
+                
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                
+              </div>
+              <div class="col-lg-3 col-xs-1 col-sm-1"></div>
+            
             </div>
-            <div class="col-lg-3 col-xs-1 col-sm-1"></div>
-           
+            
           </div>
-          
         </div>
-        <div id="footer" style={{backgroundColor: "#0D6EFD", height: "125px", width: "100%"}}>
-          <br></br>
-          <br></br>
-          <p style={{color: "#FFFFFF"}}>Developed by <a style={{color: "#FFFFFF"}} href="https://github.com/cyprste2717218">@cyprste2717218</a></p>
-        </div>
+
+        <footer>
+          <div id="footer" style={{backgroundColor: "#0D6EFD", minHeight: "20vh", width: "100%",  flexDirection: "column", }} className="d-flex justify-content-space-between">
+            <br></br>
+            <br></br>
+            <p style={{color: "#FFFFFF"}}>Developed by <a style={{color: "#FFFFFF"}} href="https://github.com/cyprste2717218">@cyprste2717218</a></p>
+          </div>
+        </footer>
+   
       </div>
     );
   }
